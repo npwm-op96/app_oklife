fba6123f8a0acde001a4e298f086104b4e8416fd
nattapon wong <nattapon-w@outlook.com>
2021-04-24 22:38:45 +0700

    fix topic assurance

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,32 +1,27 @@
 <template>
-<div class="row">
-  <v-col class="d-flex justify-center ">
-         <v-card height="500" width="500" color="">
-        <v-card-text>
-        <h1>ASSURANCE</h1>
-        </v-card-text>
-    </v-card>
-  </v-col>
-    <v-col class="d-flex justify-center ">
-         <v-card height="500" width="500" color="">
-    </v-card>
-  </v-col>
-      <v-col class="d-flex justify-center ">
-         <v-card height="500" width="500" color="">
-    </v-card>
-  </v-col>
-  
+  <div class="row">
+    <v-col class="d-flex justify-center">
+      <v-card height="500" width="500" color="">
+        <v-col class="text-left">
+          <v-card-text>
+            <h1 class="mt-6">ASSURANCE</h1>
+          </v-card-text>
+        </v-col>
+      </v-card>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <v-card height="500" width="500" color=""> </v-card>
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <v-card height="500" width="500" color=""> </v-card>
+    </v-col>
   </div>
 </template>
 <script>
-
 export default {
-    name:"Service",
-    data:()=>{
-        return{
-
-        }
-
-    }  
-}
+  name: "Service",
+  data: () => {
+    return {};
+  },
+};
 </script>
